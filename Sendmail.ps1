443feed14dc67fcab71e05912fbc0e82f61cc52f
Fsalazar504 <46978019+Fsalazar504@users.noreply.github.com>
2020-02-21 08:38:16 -0800

    Updated Sendmail.ps1
    cleared variable fields to make them more generic and usable for others.

--- Sendmail.ps1
+++ Sendmail.ps1
@@ -5,11 +5,11 @@ $PrinterStatus = Get-Printer | Where-Object PrinterStatus -like '*offline*' | Se
 
 
 #Create and convert txt to Html file
-$PrinterStatus | ConvertTo-Html | Out-File -FilePath C:\Options\Printers.txt
+$PrinterStatus | ConvertTo-Html | Out-File -FilePath C:\SomePath\Printers.txt #For "SomePath" enter a desired destination
 
 #Sends mail
-$Body = Get-Content -Path C:\Options\Printers.txt
-Send-MailMessage -From LBLPrint-Status@lbl.gov -To embedded-It@lbl.gov `
+$Body = Get-Content -Path C:\SomePath\Printers.txt
+Send-MailMessage -From PrinterAdmin@email.com -To User@email.com ` #Enter your own email fields for each @email.com variable
  -Subject 'Printer Error Report' -Body "<font size= 5><b>This is a test of printer status</b></font> <p>$Body</p>" -BodyAsHtml `
- -Priority High -DeliveryNotificationOption OnSuccess, OnFailure -SmtpServer 'smtp.lbl.gov'
+ -Priority High -DeliveryNotificationOption OnSuccess, OnFailure -SmtpServer 'smtp.server.com' #For -SmtpServer enter smtp server of choice
 
